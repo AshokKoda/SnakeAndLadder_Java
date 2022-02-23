07f58d157addbd31e9fa4b2753db6a219f4d2460
Ashok Koda <kodaashok60@gmail.com>
2022-02-24 00:14:20 +0530

    Added:Do some minor changes

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -4,12 +4,12 @@ public class SnakeLadder {
 	public static final int START_POSITION = 0;
 	public static final int IS_SNAKE = 1;
 	public static final int IS_LADDER = 2;
+	public static final int WINNING_POSITION = 100;
+	public static int currentPosition = 0;
 
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
-
 		System.out.println("<--------------------------------------------->");
-		int currentPosition = 0; // UC1 - Initialized position
 		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
 		System.out.println("Dice Number : " + diceNumber);
 		System.out.println("<--------------------------------------------->");
@@ -18,11 +18,12 @@ public class SnakeLadder {
 			currentPosition -= diceNumber;
 			if (currentPosition < 0) { // UC4 - Player restarts from 0
 				currentPosition = START_POSITION;
+				System.out.println("Snake eats you...!!!");
 			}
-			System.out.println("Player in snake position : " + currentPosition);
+			System.out.println("Player position is : " + currentPosition);
 		} else if (option == IS_LADDER) {
 			currentPosition += diceNumber;
-			System.out.println("Player in ladder position : " + currentPosition);
+			System.out.println("Player position moves : " + currentPosition);
 		} else {
 			System.out.println("No Play " + currentPosition);
 		}
