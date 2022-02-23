7c5404503eeca1a2e4862e655ee25b54fbe1a4d1
Ashok Koda <kodaashok60@gmail.com>
2022-02-23 23:34:59 +0530

    Added:UC3 - Added option check for Ladder, Snake, No Play

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -1,11 +1,27 @@
 package snakeandladder;
 
 public class SnakeLadder {
+	public static final int START_POSITION = 0;
+	public static final int IS_SNAKE = 1;
+	public static final int IS_LADDER = 2;
+
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
 
+		System.out.println("<--------------------------------------------->");
 		int currentPosition = 0; // UC1 - Initialized position
 		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
 		System.out.println("Dice Number : " + diceNumber);
+		System.out.println("<--------------------------------------------->");
+		int option = (int) ((Math.random() * 10 % 3) + 1); // UC3 - Player check options
+		if (option == IS_SNAKE) {
+			currentPosition -= diceNumber;
+			System.out.println("Player in snake position : " + currentPosition);
+		} else if (option == IS_LADDER) {
+			currentPosition += diceNumber;
+			System.out.println("Player in ladder position : " + currentPosition);
+		} else {
+			System.out.println("No Play " + currentPosition);
+		}
 	}
 }
