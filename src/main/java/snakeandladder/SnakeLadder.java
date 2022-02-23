6a8a76df7eb2a9e9ee2b41be4c274523367e7c14
Ashok Koda <kodaashok60@gmail.com>
2022-02-23 23:28:26 +0530

    Added:UC2 - Player rolls the dice to get numbers between 1 to 6

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -3,7 +3,9 @@ package snakeandladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
-		
+
 		int currentPosition = 0; // UC1 - Initialized position
+		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
+		System.out.println("Dice Number : " + diceNumber);
 	}
 }
