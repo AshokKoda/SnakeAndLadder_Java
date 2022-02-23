b880491adb1409618709914c35ca7d692de3c3d5
Ashok Koda <kodaashok60@gmail.com>
2022-02-23 23:22:22 +0530

    Added:UC1 - Initialized position

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -3,5 +3,7 @@ package snakeandladder;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
+		
+		int currentPosition = 0; // UC1 - Initialized position
 	}
 }
