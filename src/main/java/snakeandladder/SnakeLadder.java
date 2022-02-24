38682e983cb19c5b54bd7abc514a9d01fadc2b71
Ashok Koda <kodaashok60@gmail.com>
2022-02-24 12:16:27 +0530

    Added: Check Player Exact Winning Position is 100 or Not

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -4,28 +4,47 @@ public class SnakeLadder {
 	public static final int START_POSITION = 0;
 	public static final int IS_SNAKE = 1;
 	public static final int IS_LADDER = 2;
-	public static final int WINNING_POSITION = 100;
+	public static final int NO_PLAY = 0;
+	
 	public static int currentPosition = 0;
+
+	public static void playerPositionMoves() {
+		while (currentPosition < 100) {
+			System.out.println("<--------------------------------------------->");
+			int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
+			System.out.println("Dice Number : " + diceNumber);
+			
+			int option = (int) ((Math.random() * 10 % 3) + 1); // UC3 - Player check options
+			if (option == IS_SNAKE) {
+				currentPosition -= diceNumber;
+				System.out.println("Snake is bites....");
+            }
+             else if (option == IS_LADDER) {
+            	 currentPosition += diceNumber;
+            }
+            else if (option == NO_PLAY) {
+                System.out.println("NO Play");
+            }
+
+			if (currentPosition < START_POSITION) { // UC4 - Player restarts from 0
+				currentPosition = START_POSITION;
+			} else if (currentPosition > 100) {
+				currentPosition -= diceNumber;
+			}
+			System.out.println("Player moves position: " + currentPosition);
+		}
+	}
+
+	public static void playUntilWin() {
+		while (currentPosition != 100) {
+			playerPositionMoves();
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("<--------Welcome to Snake And Ladder Game---------->");
+		playUntilWin();
 		System.out.println("<--------------------------------------------->");
-		int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
-		System.out.println("Dice Number : " + diceNumber);
-		System.out.println("<--------------------------------------------->");
-		int option = (int) ((Math.random() * 10 % 3) + 1); // UC3 - Player check options
-		if (option == IS_SNAKE) {
-			currentPosition -= diceNumber;
-			if (currentPosition < 0) { // UC4 - Player restarts from 0
-				currentPosition = START_POSITION;
-				System.out.println("Snake eats you...!!!");
-			}
-			System.out.println("Player position is : " + currentPosition);
-		} else if (option == IS_LADDER) {
-			currentPosition += diceNumber;
-			System.out.println("Player position moves : " + currentPosition);
-		} else {
-			System.out.println("No Play " + currentPosition);
-		}
+		System.out.println("Player won game...");
 	}
 }
