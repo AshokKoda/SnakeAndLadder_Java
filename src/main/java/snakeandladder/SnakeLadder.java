b925e9c7ae77b1fec01a6aea43857fc8e8ce5410
Ashok Koda <kodaashok60@gmail.com>
2022-02-24 12:29:38 +0530

    Added: Check Player Exact Winning Position is 100 or Not

--- src/main/java/snakeandladder/SnakeLadder.java
+++ src/main/java/snakeandladder/SnakeLadder.java
@@ -5,7 +5,7 @@ public class SnakeLadder {
 	public static final int IS_SNAKE = 1;
 	public static final int IS_LADDER = 2;
 	public static final int NO_PLAY = 0;
-	
+
 	public static int currentPosition = 0;
 
 	public static void playerPositionMoves() {
@@ -13,18 +13,16 @@ public class SnakeLadder {
 			System.out.println("<--------------------------------------------->");
 			int diceNumber = (int) ((Math.random() * 10 % 6) + 1);// UC2 - Player rolls the dice
 			System.out.println("Dice Number : " + diceNumber);
-			
+
 			int option = (int) ((Math.random() * 10 % 3) + 1); // UC3 - Player check options
 			if (option == IS_SNAKE) {
 				currentPosition -= diceNumber;
 				System.out.println("Snake is bites....");
-            }
-             else if (option == IS_LADDER) {
-            	 currentPosition += diceNumber;
-            }
-            else if (option == NO_PLAY) {
-                System.out.println("NO Play");
-            }
+			} else if (option == IS_LADDER) {
+				currentPosition += diceNumber;
+			} else if (option == NO_PLAY) {
+				System.out.println("NO Play");
+			}
 
 			if (currentPosition < START_POSITION) { // UC4 - Player restarts from 0
 				currentPosition = START_POSITION;
